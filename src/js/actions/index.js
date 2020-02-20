dbe9f66514a0116b79c31bf3054dc7e99f7894cb
AvetisDN <avetisdn@gmail.com>
2020-02-20 18:59:58 +0300

    1

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_ARTICLE } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
 
 export function addArticle(payload) {
     return {
@@ -6,3 +6,14 @@ export function addArticle(payload) {
         payload
     }
 };
+
+export function getData() {
+    console.log('get data called')
+    return function(dispatch) {
+      return fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(json => {
+          dispatch({ type: DATA_LOADED, payload: json });
+        });
+    };
+  }
